a0927fa9ac3f12bff90373150f1b8a1351db0621
Bebolicious <a.zetterberg@hotmail.com>
2019-01-09 11:44:07 +0100

    no message

---
+++ Rememberall/Rememberall/Engine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rememberall
+{
+    class Engine
+    {
+        internal void Run()
+        {
+            LoginScreen();
+            MainMenu();
+        }
+
+        private void LoginScreen()
+        {
+            Header("Start");
+            
+        }
+
+
+        private void MainMenu()
+        {
+          
+        }
+        private void Header(string v)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();   
+            Console.WriteLine(v.ToUpper());
+            Console.WriteLine();
+        }
+
+        private void Writeline(string v)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
