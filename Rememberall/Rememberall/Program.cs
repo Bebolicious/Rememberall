7274cf54331242c377e4d4de912db81ab7a601cc
Bebolicious <a.zetterberg@hotmail.com>
2019-01-09 11:26:55 +0100

    Script Created

---
+++ Rememberall/Rememberall/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rememberall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
